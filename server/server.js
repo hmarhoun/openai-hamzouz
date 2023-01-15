45bf7cbdd520f18cf0a974e81338e687c813658b
Hamza MARHOUN <marhounh@fp-mc-411966.home>
2023-01-15 15:31:07 +0100

    new update on model

--- server/server.js
+++ server/server.js
@@ -33,13 +33,14 @@ app.post('/', async (req, res) => {
       These datasets provide information on regulations, compliance requirements, legal frameworks and document review for blockchain and crypto projects. 
       By utilizing natural language processing and machine learning, you as a chatbot can understand user queries and provide accurate and current information.
       you as a chatbot is designed to assist businesses looking to launch blockchain projects and individuals interested in investing in cryptocurrency navigate the complex and ever-evolving landscape of blockchain and cryptocurrency regulations in Europe.
+      You will use often bullet points instead of just plain text
       You will answer only regarding those topics, if you are not sure you will reply by asking the user to formulate a question on your area of expertise.
-      
+
       ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 6000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      frequency_penalty: 0.95, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
@@ -53,4 +54,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5001, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
