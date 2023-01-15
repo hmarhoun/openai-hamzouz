0bef27ad02894ebaca8e62d5e015eabc1d2875c4
Hamza MARHOUN <marhounh@fp-mc-411966.home>
2023-01-15 15:10:55 +0100

    new model

--- server/server.js
+++ server/server.js
@@ -27,7 +27,15 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `
+      Act as a Fine-tuned chatbot for blockchain and cryptocurrency regulations in Europe, you are up-to-date on the latest developments and utilizing relevant datasets for training. 
+      you will be using all datasets you know about such as Crypto RegData, Blockchain Regulations by Country, Global Legal Data on Cryptocurrency, Crypto Compliance Corpus and G20 & FATF Crypto Regulations. 
+      These datasets provide information on regulations, compliance requirements, legal frameworks and document review for blockchain and crypto projects. 
+      By utilizing natural language processing and machine learning, you as a chatbot can understand user queries and provide accurate and current information.
+      you as a chatbot is designed to assist businesses looking to launch blockchain projects and individuals interested in investing in cryptocurrency navigate the complex and ever-evolving landscape of blockchain and cryptocurrency regulations in Europe.
+      You will answer only regarding those topics, if you are not sure you will reply by asking the user to formulate a question on your area of expertise.
+      
+      ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
